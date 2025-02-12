75b1f064114bb305bf4d1bc1066044f1d4fbbaf9
Ehsan Akhavan <ehsan.akhavan@yahoo.com>
2025-02-12 14:31:58 +0330

    Add Vivado project creation script

--- Projects/alinx.sh
+++ Projects/alinx.sh
@@ -8,13 +8,19 @@ while true; do
 
 	if [[ "$response_main" == *"1"* ]]; then
 		echo "---------------Synthesize projects-----------------"
-		mkdir -p "$HDL_PROJECT/vivado"
-		cd "$HDL_PROJECT/vivado"
+		ALINX_HW=$(dirname "$HDL_PROJECT")
+		cd "$ALINX_HW"
 		HDL_PROJECT_NAME=$(basename "$HDL_PROJECT")
-		cp "$ALINX_PROJECTS/course_s2/15_pl_net/vivado/pl_net.xpr" "./$HDL_PROJECT_NAME.xpr"
-		cp -r "$ALINX_PROJECTS/course_s2/15_pl_net/vivado/pl_net.srcs" "./$HDL_PROJECT_NAME.srcs"
-		rsync -av --exclude='sim_scripts' "$ALINX_PROJECTS/course_s2/15_pl_net/vivado/pl_net.ip_user_files" .
-		find . -type f -exec sed -i "s/pl_net/compressor/g" {} +
+		# cp "$ALINX_PROJECTS/course_s2/15_pl_net/vivado/pl_net.xpr" "./$HDL_PROJECT_NAME.xpr"
+		# cp -r "$ALINX_PROJECTS/course_s2/15_pl_net/vivado/pl_net.srcs" "./$HDL_PROJECT_NAME.srcs"
+		# rsync -av --exclude='sim_scripts' "$ALINX_PROJECTS/course_s2/15_pl_net/vivado/pl_net.ip_user_files" .
+		# find . -type f -exec sed -i "s/pl_net/compressor/g" {} +
+
+		# Set Vivado environment (assuming it's installed)
+		source "$XILINX_INSTALL_DIR/settings64.sh"
+
+		# Run the TCL script in Vivado batch mode
+		vivado -mode batch -source "$PETALINUX_DIR/Scripts/Projects/compressor.tcl" -log "/home/esi/Projects/alinx_hw/log/vivado.log" -journal "/home/esi/Projects/alinx_hw/log/vivado.jou"
 
 		# export PATH="$PATH:$XILINX_INSTALL_DIR/bin"
 		# make
